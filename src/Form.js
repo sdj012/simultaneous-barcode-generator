36b6ab98cd85d2476058f4c2845a2ae0ef8e6e06
sdj012 <sdjeong012@gmail.com>
2021-02-10 03:20:59 -0500

    change input box desc. message

--- src/Form.js
+++ src/Form.js
@@ -7,7 +7,7 @@ class Form extends React.Component {
   constructor(props){
 
     super(props);
-    this.state={serialNumbers: [' : Serial Number(s) Seperated by a comma or space']}
+    this.state={serialNumbers: [' : Seperate Numbers by a comma or space']}
 
     this.handleChange=this.handleChange.bind(this);
     this.handleSubmit=this.handleSubmit.bind(this);
